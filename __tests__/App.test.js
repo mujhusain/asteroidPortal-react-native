fa7751c7785dd14f687f4842fb390b83e8f53e66
Vinayak <vinayak@metafic.co>
2022-08-03 11:25:21 +0530

    Test Pass

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -5,8 +5,19 @@ import App from '../App';
 
 jest.useFakeTimers()
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {create, act, renderer}  from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+// it('renders correctly', async () => {
+//   renderer.create(<App />);
+// });
+
+describe('<App /> Basics', () => {
+  it('has 1 child', async () => {
+    let tree
+    act(()=>{
+       tree = create(<App />)
+    })
+    expect(tree.toJSON().children.length).toBe(1);
+  });
+})
+
